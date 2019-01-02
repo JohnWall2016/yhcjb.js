3bd5e27bdfe8046d18fc4ddf439750cc65aab197
John Wall <5672618@qq.com>
2019-01-02 19:26:12 +0800

    update

--- test/session_test.js
+++ test/session_test.js
@@ -2,7 +2,7 @@
 
 const { Session, Service } = require('../lib/session');
 
-Session.using('002', s => {
+Session.use('002', s => {
     s.send(Service.Grinfo('430311195702091516'));
     console.log(s.get());
 });
