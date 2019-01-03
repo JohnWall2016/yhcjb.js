faeac4518663c849cddf53c186e6498929261988
JohnWall2016 <5672618@qq.com>
2019-01-03 14:41:07 +0800

    udpate

--- test/session_test.js
+++ test/session_test.js
@@ -1,8 +1,28 @@
 'use strict';
 
 const { Session, Service } = require('../lib/session');
+const { fromJson, Data, DyshInfo } = Service;
+
+/*
+Session.use('002', s => {
+    s.send(Service.Grinfo({idcard: '430311195702091516'}));
+    let rep = s.get();
+    console.log(rep);
+    let data = new Data(fromJson(rep).datas[0]);
+    console.log(data);
+    console.log(data.idcard);
+});
+*/
 
 Session.use('002', s => {
-    s.send(Service.Grinfo('430311195702091516'));
-    console.log(s.get());
-});
+    s.send(DyshInfo.request({
+        shzt: '0',
+        options: {
+            pagesize: 500,
+            sorting: [{"dataKey":"aaa027","sortDirection":"ascending"}]
+        }
+    }));
+    let rep = fromJson(s.get());
+    let data = new Data(rep.datas[0], DyshInfo.response);
+    console.log(data.idcard, data.retireDate);
+})
